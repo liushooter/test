d27c30b979bf9217ce616910977af03807e17d21
liushooter <shooter.hero@gmail.com>
2025-11-11 17:41:01 +0800

    ruby

---
+++ ruby/time1.rb
@@ -0,0 +1,13 @@
+utc_time = Time.now
+
+timestamp1 = utc_time.to_i
+
+puts timestamp1
+
+puts "curr time: " , Time.at(timestamp1)
+
+puts "time is " , Time.at(1762790242025/1000)
+
+puts 1+2+3
+puts(1+2+3+4)
+
